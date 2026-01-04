1e653bfb3730f5416737b7e29bef03bb2c3e6bb7
Jaden Le <ple030999@gmail.com>
2026-01-03 19:45:28 -0800

    I wrote my code for drawing a spirograph. I put two version in this time. One for my code and one for lesson's code.

--- Day-18-start/main.py
+++ Day-18-start/main.py
@@ -8,14 +8,14 @@
 #     timmy_the_turtle.forward(100)
 #     timmy_the_turtle.right(90)
 
-
+#******************************************************************************
 # #Drawing a Dashed Line
 # for _ in range(20):
 #     timmy_the_turtle.forward(10)
 #     timmy_the_turtle.penup()
 #     timmy_the_turtle.forward(10)
 #     timmy_the_turtle.pendown()
-
+#******************************************************************************
 
 # #Drawing different shapes
 # import turtle as t
@@ -35,7 +35,7 @@
 #     tim.color(random.choice(colours))
 #     draw_shape(shape_side_n)
 
-
+#******************************************************************************
 
 # #Drawing different ways
 # import turtle as t
@@ -53,38 +53,91 @@
 #     tim.forward(30)
 #     tim.setheading(random.choice(directions))
 
+#******************************************************************************
 
+# #tuple can access like the list but can not change element inside.
+# import turtle as t
+# import random
+# from turtle import Screen
+#
+# tim = t.Turtle()
+# t.colormode(255)
+#
+# def random_color():
+#     r = random.randint(0,255)
+#     g = random.randint(0,255)
+#     b = random.randint(0,255)
+#     random_color = (r,g,b)
+#     return random_color
+#
+# directions = [0,90,180,270]
+# tim.pensize(15)
+# tim.speed("fastest")
+#
+# for _ in range(200):
+#     tim.color(random_color())
+#     tim.forward(30)
+#     tim.setheading(random.choice(directions))
 
-#tuple can access like the list but can not change element inside.
+#******************************************************************************
+#**********************  Drawing a Spirograph Start ***************************
+#******************************************************************************
+# # My Code for drawing Spirograph
+# import turtle as t
+# import random
+# from turtle import Screen
+#
+# def random_color():
+#     r = random.randint(0,255)
+#     g = random.randint(0,255)
+#     b = random.randint(0,255)
+#     color = (r,g,b)
+#     return color
+#
+# tim = t.Turtle()
+# t.colormode(255)
+# tim.speed("fastest")
+# GAP = 10
+#
+# for _ in range(int(360/GAP)):
+#     tim.color(random_color())
+#     tim.circle(80)
+#     tim.left(GAP)
+#
+# screen = Screen()
+# screen.exitonclick()
+
+#******************************************************************************
+
+#Lesson Code for Drawing Spirograph
 import turtle as t
 import random
 from turtle import Screen
-
-tim = t.Turtle()
-t.colormode(255)
 
 def random_color():
     r = random.randint(0,255)
     g = random.randint(0,255)
     b = random.randint(0,255)
-    random_color = (r,g,b)
-    return random_color
+    color = (r,g,b)
+    return color
 
-directions = [0,90,180,270]
-tim.pensize(15)
+tim = t.Turtle()
+t.colormode(255)
 tim.speed("fastest")
 
-for _ in range(200):
-    tim.color(random_color())
-    tim.forward(30)
-    tim.setheading(random.choice(directions))
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360/size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
 
+draw_spirograph(5)
 
 screen = Screen()
 screen.exitonclick()
-
-
-
+#******************************************************************************
+#**********************  Drawing a Spirograph End *****************************
+#******************************************************************************
 
 #**** Lesson ******
 #Kind of import
