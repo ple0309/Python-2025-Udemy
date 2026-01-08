bb7b4dbc5d6e8ba75e08f33727e46304148bec18
Jaden Le <ple030999@gmail.com>
2026-01-07 21:28:49 -0800

    Day 21, I finished on the parts of the snake game. I learned about inheritance. I knew how to use super().__init__() from the super class which was inherited including attributes and methods of it.

---
+++ Day-20/food.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+
+        #Default will be 20x20 change into 10x10
+        self.shapesize(stretch_len=0.5, stretch_wid = 0.5)
+        self.color("blue")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
