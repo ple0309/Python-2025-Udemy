2a01c8a8da4fde7b315514c2241536bd910feb3a
Jaden Le <ple030999@gmail.com>
2026-01-10 18:13:27 -0800

    Day 24, I modified snake game project to store high score into data.txt file. It will show the high score when I run the game again.

--- Day-20/scoreboard.py
+++ Day-20/scoreboard.py
@@ -2,12 +2,14 @@ from turtle import Turtle
 
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
+FILE_NAME = "data.txt"
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open(FILE_NAME) as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0,270)
@@ -19,8 +21,10 @@ class Scoreboard(Turtle):
         self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def reset(self):
-        if self.score > self.high_score:
+        if self.score > int(self.high_score):
             self.high_score = self.score
+            with open(FILE_NAME, mode="w") as file:
+                file.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
