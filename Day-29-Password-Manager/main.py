50dd140b86f3387587f7243b5de7a9e6b7b41370
Jaden Le <ple030999@gmail.com>
2026-01-20 21:19:17 -0800

    Day 30, I worked again on previous lesson that is Day 29. In this project, I applied my new knowledge about try and except to catch error from reading and writing data in JSON file. I learned new thing about json module those are dump, load, and update method. Dump method is to add new data with dictionary style into JSON file. Load method is to read all data in JSON file. Update method is to update old data with new data after load().

--- Day-29-Password-Manager/main.py
+++ Day-29-Password-Manager/main.py
@@ -4,6 +4,12 @@ from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
 
+#import json to write in new file with json style data
+#To write (json.dump())
+#To read (json.load())
+#To update (json.update())
+import json
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
 def generate_password():
@@ -36,21 +42,60 @@ def add_data():
     website = website_entry.get()
     email = email_entry.get()
     password= password_entry.get()
+    new_data = {
+        website:{
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
-
         messagebox.showinfo(title="Oops", message="Please dont' leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: "
-                                                              f"\nEmail: {email} \nPassword: {password} \nIs it ok to save?")
-        if is_ok:
-            with open("data.txt", mode='a') as file:
-                file.write(f"{website} | {email} | {password}\n")
+        try:
+            # mode="w" if file does not exist it will create it.
+            with open("data.json", mode="r") as file:
+                # ____________________Reading the json file___________________
+                # If reading, so changing mode="r"
+                # Reading the old data
+                data = json.load(file)
+        #If the file doesn't exist.
+        except FileNotFoundError:
+            with open("data.json", mode="w") as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            # ____________________Update the json file____________________
+            # Updating old data with new data
+            data.update(new_data)
+            with open("data.json", mode="w") as file:
+                # ______Using dump method to write (object, into file)________
+                # __With indent=4 it will help for easy reading the json file.
+                # Saving updated data
+                json.dump(data, file, indent=4)
+        finally:
+            #Clear the input by take from first and last index 0 and END in this scenario.
+            website_entry.delete(0,END)
+            password_entry.delete(0,END)
+            website_entry.focus()
 
-                #Clear the input by take from first and last index 0 and END in this scenario.
-                website_entry.delete(0,END)
-                password_entry.delete(0,END)
-                website_entry.focus()
+# --------------------------- FIND PASSWORD --------------------------- #
+def find_password():
+    website = website_entry.get()
+    if len(website) == 0:
+        messagebox.showinfo(title="Oops", message="Empty input!")
+    else:
+        try:
+            with open("data.json",mode="r") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            messagebox.showinfo(title="Error", message="No Data File Found.")
+        else:
+            if website in data:
+                email = data[website_entry.get()]['email']
+                password = data[website_entry.get()]['password']
+                messagebox.showinfo(title=f"{website_entry.get()}", message=f"Email: {email}\nPassword: {password}")
+            else:
+                messagebox.showinfo(title="Error", message=f"No details for {website} exits.")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -76,8 +121,8 @@ password_label = Label(text="Password:")
 password_label.grid( row=3,column=0)
 
 #Entry
-website_entry = Entry(width=52)
-website_entry.grid(row=1, column=1,columnspan=2)
+website_entry = Entry(width=33)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 
 email_entry = Entry(width=52)
@@ -90,6 +135,9 @@ password_entry = Entry(width=33)
 password_entry.grid(row=3,column=1)
 
 #Button
+search_button = Button(text="Search", width=14, command=find_password)
+search_button.grid(row=1, column = 2)
+
 generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid( row=3,column=2)
 
