aa9df22f26ec00fc926e9e0d7754477ff72f06c8
Jaden Le <ple030999@gmail.com>
2026-02-12 00:34:39 -0800

    Day 55, I learned new things based on the day 54, so I worked on it together. In this lesson, I learned about how to create another decorator with *args and **kwagrs. At the end of the lesson, I also have a small project to create the guessing number webpage. I will use the number from app.route('/<int:guess>') to work with guess variable from user, then printing out the html form based on the result.

--- Day-54 Introduction with Flask/hello_flask/hello.py
+++ Day-54 Introduction with Flask/hello_flask/hello.py
@@ -5,14 +5,51 @@ print(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return ('<h1 style="text-align:center">Hello, World!</h1>'
+            '<p>This is a paragraph</p>'
+            '<img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGI3ajVydHFhOThta2R0YW43OWd2Y29ieWcxM2xscjIwbzVvaTBnbyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/KLB8IXYVZevYY/giphy.gif" width=200px>')
 
+#Different routes using the app.rout decorator
+#u = Underline
+#em = Italic
+#b = Bold
+def make_bold(func):
+    def wrapper():
+        return "<b>" + func() + "</b>"
+    return wrapper
+
+def make_emphasis(func):
+    def wrapper():
+        return "<em>" + func() + "</em>"
+    return wrapper
+
+def make_underlined(func):
+    def wrapper():
+        return "<u>" + func() + "</u>"
+    return wrapper
 @app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underlined
 def say_bye():
-    return "Bye"
+    return "Bye!"
+
+#/<name> is the variable so all the string after username/ will be the name in this case
+# If I want to do with other variable, I can do it with the converter types int
+# also put number in parameter.
+@app.route("/username/<name>/<int:number>")
+def greet(name, number):
+    return f"Hello there {name}, you are {number} years old."
+
+# #Another example about if I want to get the path inside variable.
+# #path:name inside <> will return all the path with slash after username/
+# @app.route("/username/<path:name>")
+# def greet(name):
+#     return f"Hello there {name}"
 
 if __name__ == "__main__":
-    app.run()
+    #run the app in debug mode to auto-reload.
+    app.run(debug=True)
 
 
 
@@ -98,3 +135,25 @@ if __name__ == "__main__":
 #
 # decorated_function = delay_decorator(say_greeting)
 # decorated_function()
+
+
+###############################################################
+## Advanced Python Decorator Functions with *args and **kwargs
+class User:
+    def __init__(self,name):
+        self.name = name
+        self.is_logged_in = False
+
+def is_authenticated_decorator(function):
+    def wrapper(*args, **kwargs):
+        if args[0].is_logged_in == True:
+            function(args[0]) #put the args[0] like user in this case.
+    return wrapper
+
+@is_authenticated_decorator
+def create_blog_post(user):
+    print(f"This is {user.name}'s new blog post.")
+
+new_user = User("Jaden")
+new_user.is_logged_in = True
+create_blog_post(new_user)
