20d9ca72ba2c07de1b10412e687e2f8adfbd7fef
Jaden Le <ple030999@gmail.com>
2026-02-15 15:52:36 -0800

    Day 61, I learned about WTF form for log in with email and password. I also use bootstrap-flaskk to use the method render_form(form) and bootstrap.load_css for styling. In addition, I know how to create a template like base.html, then using jinja to apply for another html files with the format of base.html. While doing it, I have to combine jinja style block and bootstrap form for better webpage.

---
+++ Day-61 WFT form and Bootstrap Flask/main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from myform import MyForm
+from flask_bootstrap import Bootstrap5
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+class LoginForm(FlaskForm):
+    email = StringField(label="Email")
+    password = PasswordField(label="Password")
+    submit = SubmitField(label="Log In")
+
+app = Flask(__name__)
+app.secret_key = "test1"
+bootstrap = Bootstrap5(app)
+app.secret_key = "test1"
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    form = MyForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+
+    return render_template('login.html',form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
